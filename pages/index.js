8cda726f6179ce165c0d206a2a4b54b8ff497f35
azzahra-jateng <azzahra.jateng@gmail.com>
2021-08-26 21:09:49 +0700

    upadate

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
-        <h1>Hallo Nextjs</h1>
+        <h1>Hallo Nextjs Update</h1>
     </div>
   )
 }
